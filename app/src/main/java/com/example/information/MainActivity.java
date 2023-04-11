e38f3c574a3983a25e52fe56beea02ece700a4c6
JS-404-000 <ksok345@naver.com>
2023-04-11 15:25:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/information/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.information;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult, tvResultLabal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnReq = findViewById(R.id.btnRequest);
+        Button btnEnd = findViewById(R.id.btnEnd);
+        tvResult = findViewById(R.id.tVResvlt);
+        tvResultLabal = findViewById(R.id.tVResultLabel);
+        EditText editText= findViewById(R.id.editTextID);
+        ///...
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
